7bd88e1cd4c9bc4d6726a74388e6df7ad672244a
superscripted#0201 <80987982+supercellgamer@users.noreply.github.com>
2021-09-28 18:17:10 -0600

    Update IslandsUI.lua

--- IslandsUI.lua
+++ IslandsUI.lua
@@ -213,7 +213,7 @@ do
 	
 	function library.new(title)
 		local container = utility:Create("ScreenGui", {
-			Name = title,
+			Name = "IslandsGui",
 			Parent = game.CoreGui
 		}, {
 			utility:Create("ImageLabel", {
